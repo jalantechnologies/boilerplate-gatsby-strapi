1d8a3710b4a33d9dbb4b6d11ab6889e364bec362
agentraghav <raghvendrarathore2@gmail.com>
2022-11-02 19:51:33 +0530

    strapi ts setup

---
+++ api/config/plugin.ts
@@ -0,0 +1,18 @@
+// path: ./config/plugins.ts
+
+export default {
+    //
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: false,
+        depthLimit: 7,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+      },
+    },
+  };
+  
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,36 @@ import { SEO } from '../components'
 import { Client as Styletron } from 'styletron-engine-atomic'
 import { Provider as StyletronProvider } from 'styletron-react'
 import { LightTheme, BaseProvider } from 'baseui'
+import { graphql, useStaticQuery } from 'gatsby'
 import {
-    DisplayMedium,
+    DisplayMedium, DisplayXSmall
   } from 'baseui/typography';
 
 const engine = new Styletron()
 
 const IndexPage = () => {
+
+  const { strapiProduct } = useStaticQuery(
+    graphql`
+      query {
+        strapiProduct {
+          name
+          slug
+        }
+      }
+    `
+  )
+
   return (
     <StyletronProvider value={engine}>
       <BaseProvider theme={LightTheme}>
         <SEO title="home" />
         <DisplayMedium>
-            Gatsby Strapi Boilerplate
+            {strapiProduct.name}
         </DisplayMedium>
+        <DisplayXSmall>
+            {strapiProduct.slug}
+        </DisplayXSmall>
       </BaseProvider>
     </StyletronProvider>
   )
